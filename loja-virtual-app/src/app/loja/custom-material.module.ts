f7f1b49515d5feedb9a758f7aa7dd705f4caadc4
Everson Delmaschio <eversondelmaschio@gmail.com>
2018-06-30 20:39:05 +0000

    tela de pagamento iniciada

--- loja-virtual-app/src/app/loja/custom-material.module.ts
+++ loja-virtual-app/src/app/loja/custom-material.module.ts
@@ -3,10 +3,11 @@ import {
     MatButtonModule, MatCheckboxModule, MatCardModule
 } from '@angular/material';
 import { ProdutoDetalheComponent } from './produto-detalhe/produto-detalhe.component';
+import { PagamentoComponent } from './pagamento/pagamento.component';
 
 @NgModule({
     imports: [MatButtonModule, MatCheckboxModule, MatCardModule],
     exports: [MatButtonModule, MatCheckboxModule, MatCardModule],
-    declarations: [ProdutoDetalheComponent]
+    declarations: [ProdutoDetalheComponent, PagamentoComponent]
 })
 export class CustomMaterialModule { }
